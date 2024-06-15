55b8eb74357573dae59d50c8007241defb30865a
Kanumuri Sri Charan <royalcharan2002@gmail.com>
2024-06-15 12:33:05 +0530

    Assignment code added

---
+++ profiles.py
@@ -0,0 +1,14 @@
+def GetAdityaName():
+    return "Aditya N M"
+
+while True:
+    Argument= input()
+    
+    if Argument == "Aditya":
+        print(GetAdityaName())
+    
+    elif Argument == "exit":
+        break
+    
+    else:
+        print("Invalid argument")
